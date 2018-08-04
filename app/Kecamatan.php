34dbd9ba248d1188d764608e5572d8fed8f79717
abialemuel <abia.darma@bukalapak.com>
2018-08-04 18:04:47 +0700

    add model location

---
+++ app/Kecamatan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kecamatan extends Model
+{
+    //
+    protected $table = 'lokasi_kecamatan';
+    protected $guarded = [ 'id' ];
+}
---
+++ app/Kelurahan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelurahan extends Model
+{
+    //
+    protected $table = 'lokasi_kelurahan';
+    protected $guarded = [ 'id' ];
+}
