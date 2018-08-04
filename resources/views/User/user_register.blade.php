5cc76c2205c63961cef0a0749b93d5e60a868b1e
Krian Adhinata <krianbayu4@gmail.com>
2018-08-05 00:38:09 +0700

    update user_register

--- resources/views/User/user_register.blade.php
+++ resources/views/User/user_register.blade.php
@@ -30,97 +30,7 @@
 	<script src="user/js/vendor/custom.modernizr.js"></script>
 </head>
 <body>
-
-<!-- Begin Main Wrapper -->
-<div class="main-wrapper">
-	<!-- Main Navigation -->
-	<header class="row main-navigation">
-		<div class="large-3 columns">
-			<a href="index.html" id="logo"><img src="user/images/logo.png" alt="Medico Logo" /></a>
-		</div>
-		<div class="large-9 columns">
-			<nav class="top-bar">
-				<ul class="title-area">
-				  <!-- Title Area -->
-				  <li class="name"></li>
-				  <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-				  <li class="toggle-topbar menu-icon"><a href="#"><span>Main Menu</span></a></li>
-				</ul>
-
-				<section class="top-bar-section">
-					<!-- Left Nav Section -->
-					  <ul class="right">
-						<li class="has-dropdown"><a href="#" class="active">Home</a>
-						  <ul class="dropdown">
-							<li><a href="index2.html">Variation 2</a></li>
-							<li><a href="index3.html">Variation 3</a></li>
-							<li><a href="index4.html">Variation 4</a></li>
-							<li><a href="index5.html">Variation 5</a></li>
-							<li><a href="index6.html">Variation 6</a></li>
-						  </ul>
-						</li>
-						<li class="has-dropdown"><a href="#">Features</a>
-						  <ul class="dropdown">
-                          	<li><a href="about.html">About page</a></li>
-							<li><a href="grid.html">Grid</a></li>
-							<li><a href="navigation.html">Navigation</a></li>
-							<li><a href="buttons.html">Buttons</a></li>
-							<li><a href="forms.html">Forms</a></li>
-							<li><a href="typography.html">Typography</a></li>
-							<li><a href="orbit.html">Orbit - Foundation Slider</a></li>
-							<li><a href="clearing.html">Clearing -  Responsive Lightboxes</a></li>
-							<li><a href="dropdown.html">Dropdown</a></li>
-							<li><a href="joyride.html">Joyride</a></li>
-							<li><a href="magellan.html">Magellan</a></li>
-							<li><a href="reveal.html">Reveal - Modal dialogs / pop-up windows</a></li>
-							<li><a href="section.html">Section - Accordion, Tabs &amp; Vertical Nav</a></li>
-							<li><a href="tooltips.html">Info tooltips</a></li>
-						  </ul>
-						</li>
-
-						<li class="has-dropdown"><a href="#">Shortcodes</a>
-						  <ul class="dropdown">
-                          	<li><a href="shortcodes.html">Main elements</a></li>
-							<li><a href="alert-boxes.html">Alert Boxes</a></li>
-							<li><a href="panels.html">Panels</a></li>
-							<li><a href="pricing-tables.html">Pricing Tables</a></li>
-							<li><a href="bars.html">Progress Bars</a></li>
-							<li><a href="tables.html">Tables</a></li>
-							<li><a href="thumbnails.html">Thumbnails</a></li>
-							<li><a href="flex-video.html">Flex Video</a></li>
-
-						  </ul>
-						</li>
-						<li class="has-dropdown"><a href="#">Add-ons</a>
-						  <ul class="dropdown">
-							<li><a href="icons.html">Retina Icons - Font Awesome</a></li>
-						  </ul>
-						</li>
-						<li class="has-dropdown"><a href="#">Portfolio</a>
-						  <ul class="dropdown">
-                          		<li><a href="portfolio.html">Portfolio 4 Columns</a> </li>
-								<li><a href="portfolio2.html">Portfolio 3 Columns</a></li>
-								<li><a href="portfolio-single.html">Single item</a></li>
-							</ul>
-						</li>
-						<li class="has-dropdown"><a href="#">Blog</a>
-						  <ul class="dropdown">
-							<li><a href="blog.html">1 Column, Default</a></li>
-							<li><a href="blogalt.html">1 Column, Alternative</a></li>
-							<li><a href="blog-2column.html">2 Columns</a></li>
-							<li><a href="blog-3column-no-sidebar.html">3 Columns, No sidebar</a></li>
-							<li><a href="blog-4column-no-sidebar.html">4 Columns, No sidebar</a></li>
-							<li><a href="blog-single.html">Blog Inner</a></li>
-						  </ul>
-						</li>
-						<li><a href="contact.html">Contact</a></li>
-					  </ul>
-					  <!-- End Left Nav Section -->
-				</section>
-			</nav>
-		</div>
-	</header>
-</div><!-- End Main Navigation -->
+@include('user/header')
 <div class="main-content-top">
 	<div class="main-wrapper">
 		<div class="row">
@@ -147,6 +57,19 @@
           <form class="custom">
 						<fieldset>
 							<legend>Data Pasien</legend>
+							<div class="row">
+								<div class="large-6 columns">
+									<label for="customDropdown3">ID</label>
+									<select id="customDropdown3" class="medium">
+										<option>KTP</option>
+										<option>SIM</option>
+									</select>
+								</div>
+								<div class="large-6 columns">
+									<label>Nomor ID (Nomor KTP, Nomor SIM)</label>
+									<input type="text" placeholder="Isikan dengan nama pasien">
+								</div>
+							</div>
 							<div class="row">
 								<div class="large-12 columns">
 									<label>Nama Pasien</label>
@@ -193,7 +116,7 @@
 							</div>
 							<div class="row">
 								<div class="large-4 columns">
-									<label>kewarganegaraan</label>
+									<label>Kewarganegaraan</label>
 									<input type="text" placeholder="ISIKAN SESUAI KEWARGANEGARAAN PASIEN">
 								</div>
 								<div class="large-4 columns">
