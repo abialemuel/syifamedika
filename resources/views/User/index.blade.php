d1b0e894f67d2a9e353255f1fdf5004b6c2e81b7
Krian Adhinata <krianbayu4@gmail.com>
2018-08-04 22:31:30 +0700

    update user_register

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width" />
-	<title>Syifa Medika - Beranda</title>
+	<title>Beranda - RS Syifa Medika</title>
 	<link rel="icon" href="user/images/logo1.png">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
 	<!-- Smallipop CSS - Tooltips -->
@@ -39,54 +39,7 @@
 <!-- Begin Main Wrapper -->
 <div class="main-wrapper">
 	<!-- Main Navigation -->
-	<header class="row main-navigation">
-		<div class="large-3 columns">
-			<a href="index.html" id="logo"><img src="user/images/logo.png" alt="Medico Logo" /></a>
-		</div>
-		<div class="large-9 columns">
-			<nav class="top-bar">
-				<ul class="title-area">
-				  <!-- Title Area -->
-				  <li class="name"></li>
-				  <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-				  <li class="toggle-topbar menu-icon"><a href="#"><span>Main Menu</span></a></li>
-				</ul>
-
-				<section class="top-bar-section">
-					<!-- Left Nav Section -->
-					  <ul class="right">
-						<li class=""><a href="#" class="active">Beranda</a></li>
-						<li class="has-dropdown"><a href="#">Profil</a>
-						  <ul class="dropdown">
-							<li><a href="profilrs">Profil Rumah Sakit</a></li>
-							<li><a href="dokter">Profil Dokter</a></li>
-							<li><a href="navigation.html">Dokter Spesialis</a></li>
-						  </ul>
-						</li>
-						<li class="has-dropdown"><a href="#">Rawat Jalan</a>
-						  <ul class="dropdown">
-            	<li><a href="shortcodes.html">Klinik Mata</a></li>
-							<li><a href="alert-boxes.html">Klinik Gigi</a></li>
-							<li><a href="panels.html">Klinik Jantung & Pembuluh Darah</a></li>
-							<li><a href="pricing-tables.html">Klinik Anak</a></li>
-							<li><a href="bars.html">Klinik Bedah</a></li>
-							<li><a href="tables.html">Klinik Syaraf</a></li>
-							<li><a href="thumbnails.html">Klinik Rehabilitasi Medis</a></li>
-							<li><a href="flex-video.html">Klinik Penyakit Dalam</a></li>
-							<li><a href="bars.html">Klinik Kebidanan dan Kandungan</a></li>
-							<li><a href="tables.html">Klinik Umum</a></li>
-						  </ul>
-						</li>
-						<li class=""><a href="rawatinap">Rawat Inap</a></li>
-						<li><a href="layananbpjs">Layanan BPJS</a></li>
-
-					  </ul>
-					  <!-- End Left Nav Section -->
-				</section>
-			</nav>
-		</div>
-	</header>
-
+	<?php @include('header.blade.php')?>
 </div>
 
 <div class="slider-wrapper theme-default">
